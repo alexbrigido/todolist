d7d04260f7bfbf2f0fa413fad76fb7ebe37f344c
ALEXANDRE DIAS BRIGIDO <alexbrigido@hotmail.com>
2021-06-20 19:19:43 -0300

    added register view

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './src/screens/App';
 import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => Register);
